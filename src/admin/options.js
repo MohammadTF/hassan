fa880d16d4d593df3862d14a5b8ec40e7e9a6256
mfaizyab <mfaizyab@live.com>
2021-04-11 12:06:37 +0500

    removed login text

--- src/admin/options.js
+++ src/admin/options.js
@@ -7,20 +7,30 @@ const StudentOptions = require('../entities/Students/options');
 const NotificationOptions = require('../entities/Notifications/options');
 const DocumentsOptions = require('../entities/Documents/options');
 
+const locale = {
+  translations: {
+    labels: {
+      // change Heading for Login
+      loginWelcome: '',
+    },
+    messages: {
+      loginWelcome: '',
+    },
+  },
+};
 /** @type  {AdminBro.AdminBroOptions} */
 const options = {
   resources: [StudentOptions, NotificationOptions, DocumentsOptions],
   rootPath: '/admin',
+  locale,
   branding: {
     companyName: 'Student Portal',
     logo: 'https://res.cloudinary.com/dmv8gen17/image/upload/v1618037782/DNMS_LOGO_stmhw1.png',
     softwareBrothers: false,
   },
   dashboard: {
-    handler: async () => {
-      return { some: 'output' }
-    },
-    component: AdminBro.bundle('./components/my-dashboard-component')
+    handler: async () => ({ some: 'output' }),
+    component: AdminBro.bundle('./components/my-dashboard-component'),
   },
 };
 
